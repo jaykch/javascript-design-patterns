7927b2a88d761cc9613228f87a7ad01a62c4f0ed
Jay Kumar <jaykch@outlook.com>
2016-10-20 22:45:33 +0100

    Added more methods

--- js/app.js
+++ js/app.js
@@ -4,10 +4,28 @@ var log = function (x) {
     console.log(x);
 };
 
+const initialPlayerXPos = 200;
+const initialPlayerYPos = 380;
+const playerLeftMoveCount = -100;
+const playerRightMoveCount = 100;
+const playerUpMoveCount = -83;
+const playerDownMoveCount = 83;
+
+const xPlayerBounds = [0,400];
+const yPlayerBounds = [0, 380];
+
+const xEnemyBounds = [-80,500];
+
+const enemyRowPos = [60, 143, 226];
+
 //todo: maybe randomize it in a method
 
-var randoomSpeed = function () {
+var randomSpeed = function () {
     return Math.floor((Math.random() * 200) + 50);
+};
+
+var randomEnemyPosition = function () {
+
 };
 
 var Enemy = function (row) {
@@ -17,22 +35,27 @@ var Enemy = function (row) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.speed = randoomSpeed();
-    this.x = Math.floor((Math.random() * 350) + 1);
+    this.speed = randomSpeed();
+    this.x = this.setPosition();
 
     //switch statement to select each row by parsing in an argument
     //position for each row
     switch (row) {
         case 1:
-            this.y = 60;
+            this.y = enemyRowPos[0];
             break;
         case 2:
-            this.y = 143;
+            this.y = enemyRowPos[1];
             break;
         case 3:
-            this.y = 226;
+            this.y = enemyRowPos[2];
             break;
     }
+};
+
+//todo: design setters maybe
+Enemy.prototype.setPosition = function () {
+    return Math.floor((Math.random() * 350) + 1);
 };
 
 // Update the enemy's position, required method for game
@@ -41,11 +64,11 @@ Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.speed * dt;// Math.floor((Math.random() * 10) + 1);
+    this.x += this.speed * dt;// Math.floor((Math.random() * 10) + 1);
     //if statement to reset the position of the bug once it reaches the boundaries
-    if (this.x > 500) {
-        this.x = -80;
-        this.speed = randoomSpeed();
+    if (this.x > xEnemyBounds[1]) {
+        this.x = xEnemyBounds[0];
+        this.speed = randomSpeed();
     }
 };
 
@@ -58,26 +81,24 @@ Enemy.prototype.render = function () {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function () {
-    this.x = 200;
-    this.y = 380;
+    this.x = initialPlayerXPos;
+    this.y = initialPlayerYPos;
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function () {
     //todo: add methods for resetting position
-    if (this.y > 400 || this.y < 0) {
-        this.y = 380;
-        this.x = 200;
-    }
-    //log("updating");
-    if (this.x < 0) {
-        this.x = 0;
-    }
-    else if (this.x > 400) {
-        this.x = 400;
-    }
-    else if (this.y < 0) {
-        this.y = 0;
+
+};
+
+Player.prototype.reset = function (coordinate) {
+    switch (coordinate) {
+        case 'x':
+            this.x = initialPlayerXPos;
+            break;
+        case 'y':
+            this.y = initialPlayerYPos;
+            break;
     }
 };
 
@@ -85,21 +106,44 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function (direction) {
-    //todo: build methods move left and move right
+//move method for moving the player in each direction
+Player.prototype.move = function (direction) {
     switch (direction) {
         case 'left':
-            this.x += -100;
+            this.x += playerLeftMoveCount;
             break;
         case 'right':
-            this.x += 100;
+            this.x += playerRightMoveCount;
             break;
         case 'up':
-            this.y += -83;
+            this.y += playerUpMoveCount;
             break;
+
         case 'down':
-            this.y += 83;
+            this.y += playerDownMoveCount;
             break;
+    }
+};
+
+Player.prototype.handleInput = function (direction) {
+    //use the move method to move the player
+    this.move(direction);
+    //resetting player position if it hits a wall
+    if (this.y < yPlayerBounds[0]) {
+        this.reset('y');
+        this.reset('x');
+    }
+    //log("updating");
+    if (this.x < xPlayerBounds[0]) {
+        this.x = xPlayerBounds[0];
+        log(this.x);
+    }
+    else if (this.x > xPlayerBounds[1]) {
+        this.x = xPlayerBounds[1];
+        log(this.x);
+    }
+    else if (this.y > yPlayerBounds[1]) {
+        this.reset('y');
     }
 };
 
